78d715edefa298a05854f63a94bfe7478ff4b031
loinos <PavlovskiyD.V@yandex.ru>
2023-10-21 12:51:45 +0300

    server fix

--- forklifts-massager/app_main.py
+++ forklifts-massager/app_main.py
@@ -17,12 +17,12 @@ def generate_activ():
     # list to store all warehouses
     warehouses = []
     # parallel all tasks by approx 80% of CPUs
-    for i in range(int(multiprocessing.cpu_count() * 0.2)):
+    for i in range(1):
         # create new warehouse with random city from cities list
         warehouses.append(Warehouse(warehouse_id=i, city=cities[randint(0, len(cities) - 1)]))
     # here we use ProcessPool from pathos lib.
     # The reason why we don't use standard multiprocessing lib is because it does not support classes pickling
-    pool = ProcessPool(nodes=int(multiprocessing.cpu_count() * 0.2))  # we use 80% of cores
+    pool = ProcessPool(nodes=1)  # we use 80% of cores
     pool.map(run_warhouse, warehouses)
 
     return 0
